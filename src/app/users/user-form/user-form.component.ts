a64fd1a785d69de0dae7101f231a39b77ea31acc
Eduardo Mandujano <jesusgutierrez191105@gmail.com>
2024-09-11 08:15:42 -0600

    Add Conection and Styles

--- src/app/users/user-form/user-form.component.ts
+++ src/app/users/user-form/user-form.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, SimpleChanges, Output } from '@angular/core';
+import { IUser } from '../modelo/iuser';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.css'
+  styleUrls: ['./user-form.component.css']
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnChanges {
+  @Output() userAdded = new EventEmitter<IUser>();
+  @Input() maxId: number = 0;
 
+  user: IUser = {
+    id: 0,
+    name: "",
+    username: "",
+    phone: "",
+    website: ""
+  };
+
+  agregar(): void {
+    const newUser: IUser = { ...this.user, id: this.maxId + 1 };
+    this.userAdded.emit(newUser);
+    this.user = { id: 0, name: "", username: "", phone: "", website: "" };
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['maxId']) {
+      this.maxId = changes['maxId'].currentValue;
+    }
+  }
 }
--- src/app/users/users-dashboard/users-dashboard.component.ts
+++ src/app/users/users-dashboard/users-dashboard.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { IUser } from '../modelo/iuser';
 
 @Component({
   selector: 'app-users-dashboard',
   templateUrl: './users-dashboard.component.html',
-  styleUrl: './users-dashboard.component.css'
+  styleUrls: ['./users-dashboard.component.css'],
 })
 export class UsersDashboardComponent {
+  user_list: IUser[] = [];
+  maxId: number = 0;
 
+  constructor(private _service: UserService) {
+    this._service.getAll().subscribe(
+      response => {
+        this.user_list = response;
+        this.maxId = this.user_list.length > 0 ? Math.max(...this.user_list.map(user => user.id)) : 0;
+      }
+    );
+  }
+
+  onUserAdded(newUser: IUser): void {
+    this.user_list.push(newUser);
+  }
 }
